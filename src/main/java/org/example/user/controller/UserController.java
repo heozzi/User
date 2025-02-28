0b28ba071e9b4162b870a09be20ba8657a808429
yun0905 <loona17@naver.com>
2025-02-28 10:03:39 +0900

    오타 수정

--- src/main/java/org/example/user/controller/UserController.java
+++ src/main/java/org/example/user/controller/UserController.java
@@ -135,7 +135,7 @@ public class UserController {
         }
     }
 
-    // 테스트
+    // 테스트용
     @PostMapping("/checkemail")
     public Long checkemail(@RequestBody UserDto userDto) {
         System.out.println("이메일 pid 확인 "+userDto.getEmail());
