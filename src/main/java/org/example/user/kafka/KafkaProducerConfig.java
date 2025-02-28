b3d25d210fb6e0b280c46870ae5af90a819829f9
heozzi <hz0819@naver.com>
2025-02-28 18:17:01 +0900

    CI/CD Kafka 수정

--- src/main/java/org/example/user/kafka/KafkaProducerConfig.java
+++ src/main/java/org/example/user/kafka/KafkaProducerConfig.java
@@ -15,13 +15,12 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        // Kafka 브로커 주소 (환경에 맞게 수정)
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.212.241.179:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        // 필요에 따라 추가 설정 (예: ack, retries 등)
         return new DefaultKafkaProducerFactory<>(configProps);
     }
+
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
